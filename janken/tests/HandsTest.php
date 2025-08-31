c15e5cee8560704e42982efa55d29f49e561879c
sakura <d1060tsu0tarow@gmail.com>
2025-08-31 22:56:07 +0900

    テストにコメントを追加 必要なファイルをgit管理する

--- janken/tests/HandsTest.php
+++ janken/tests/HandsTest.php
@@ -7,14 +7,30 @@ use PHPUnit\Framework\Attributes\DataProvider;
 use Enum\Hands;
 use Enum\Result;
 
+/**
+ * Enum/Handsで作成したメソッドのテスト
+ *
+ * @covers Enum\Hands
+ */
 class HandsTest extends TestCase
 {
+    /**
+     * getHandTextメソッドの正常系テスト
+     *
+     * @dataProvider handsProvider
+     * @covers Enum\Hands::getHandText
+     */
     #[DataProvider('handsProvider')]
-    public function testGetResultText(Hands $hand, string $expected)
+    public function testGetResultText(Hands $hand, string $expected): void
     {
         $this->assertSame($expected, $hand->getHandText());
     }
 
+    /**
+     * getHandTextメソッドの入力データ
+     *
+     * @return array<Hands,string>
+     */
     public static function handsProvider(): array
     {
         return [
@@ -24,12 +40,23 @@ class HandsTest extends TestCase
         ];
     }
 
+    /**
+     * winHandsメソッドの正常系テスト
+     *
+     * @dataProvider winHandsProvider
+     * @covers Enum\Hands::winHands
+     */
     #[DataProvider('winHandsProvider')]
-    public function testWinHands(Hands $hand, Hands $expected)
+    public function testWinHands(Hands $hand, Hands $expected): void
     {
         $this->assertSame($expected, $hand->winHands());
     }
 
+    /**
+     * winHandsProviderメソッドの入力データ
+     *
+     * @return array<Hands,Hands>
+     */
     public static function winHandsProvider(): array
     {
         return [
@@ -39,19 +66,35 @@ class HandsTest extends TestCase
         ];
     }
 
-    public function testJudgeDraw()
+    /**
+     * judgeメソッドであいこになった場合のテスト
+     *
+     * @covers Enum\Hands::winHands
+     */
+    public function testJudgeDraw(): void
     {
         foreach (Hands::cases() as $hand) {
             $this->assertSame(Result::Draw, $hand->judge($hand));
         }
     }
 
+    /**
+     * judgeメソッドでユーザーが勝った場合のテスト
+     *
+     * @dataProvider judgeWinProvider
+     * @covers Enum\Hands::winHands
+     */
     #[DataProvider('judgeWinProvider')]
-    public function testJudgeWin(Hands $player_hands, Hands $computer_hands, Result $expected)
+    public function testJudgeWin(Hands $player_hands, Hands $computer_hands, Result $expected): void
     {
         $this->assertSame($expected, $player_hands->judge($computer_hands));
     }
 
+    /**
+     * judgeメソッドでユーザーが勝った場合の入力データ
+     *
+     * @return array<Hands,Hands,Result>
+     */
     public static function judgeWinProvider(): array
     {
         return [
@@ -61,12 +104,23 @@ class HandsTest extends TestCase
         ];
     }
 
+    /**
+     * judgeメソッドでユーザーが負けた場合のテスト
+     *
+     * @dataProvider judgeLoseProvider
+     * @covers Enum\Hands::winHands
+     */
     #[DataProvider('judgeLoseProvider')]
-    public function testJudgeLose(Hands $player_hands, Hands $computer_hands, Result $expected)
+    public function testJudgeLose(Hands $player_hands, Hands $computer_hands, Result $expected): void
     {
         $this->assertSame($expected, $player_hands->judge($computer_hands));
     }
 
+    /**
+     * judgeメソッドでユーザーが負けた場合の入力データ
+     *
+     * @return array<Hands,Hands,Result>
+     */
     public static function judgeLoseProvider(): array
     {
         return [
--- janken/tests/ResultTest.php
+++ janken/tests/ResultTest.php
@@ -5,14 +5,30 @@ use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\Attributes\DataProvider;
 use Enum\Result;
 
+/**
+ * Enum/Resultで作成したメソッドのテスト
+ *
+ * @covers Enum\Result
+ */
 class ResultTest extends TestCase
 {
+    /**
+     * getResultTextメソッドの正常系テスト
+     *
+     * @dataProvider resultProvider
+     * @covers Enum\Result::getResultText
+     */
     #[DataProvider('resultProvider')]
-    public function testGetResultText(Result $result, string $expected)
+    public function testGetResultText(Result $result, string $expected): void
     {
         $this->assertSame($expected, $result->getResultText());
     }
 
+    /**
+     * getResultTextメソッドの入力データ
+     *
+     * @return array<Result,string>
+     */
     public static function resultProvider(): array
     {
         return [
